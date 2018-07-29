e997f8a926325cb041562cc155e5c10360157812
Anton Mac <antony.mayer@gmail.com>
2018-07-28 22:49:18 -0500

    ref

--- app.js
+++ app.js
@@ -1,83 +1,41 @@
-const Wit     = require('wit-cms');
-const express = require('express');
-const favicon = require('serve-favicon');
-const hljs    = require('highlight.js');
-const lodash  = require('lodash');
-const logger  = require('morgan');
-const path    = require('path');
-var app       = express();
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
+
+var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
+app.set('view engine', 'jade');
 
-// uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
 
-// only use the static-file middleware on the dev environment
-if ('development' == app.get('env')) {
-  app.use(express.static(path.join(__dirname, 'public')));
-  app.use('/media', express.static('media'));
-}
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
 
-// wit configs
-const config = {
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
 
-  // website metadata, a la Wordpress
-  params: {
-    author   : 'John Doe',
-    fqdn     : 'https://example.com',
-    name     : 'example.com',
-    tagLine  : 'Built with wit-bootstrap',
-    appStart : Date.now(), // for stylesheet cache-busting
-  },
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // syntax highlighting configuration
-  remarkable: {
-    highlight: function (str, lang) {
-      if (lang && hljs.getLanguage(lang)) {
-        try {
-          return hljs.highlight(lang, str).value;
-        } catch (err) {}
-
-      }
-      try {
-        return hljs.highlightAuto(str).value;
-      } catch (err) {}
-      return '';
-    }
-  },
-};
-
-// init the app
-Wit(app, config, function(err, wit) {
-
-  // extract years for blog archives
-  wit.params.years = lodash(wit.posts)
-    .map('date.year')
-    .uniq()
-    .sort()
-    .reverse()
-    .value();
-  
-  // redirect the home page to the blog
-  app.get('/', function(req, res) {
-    res.redirect(301, '/blog');
-  });
-
-  // implement a 404 page
-  app.use(function(req, res, next) {
-    res.status(404).render('404', {
-      page : {
-        title     : 'Not Found',
-        name      : 'Not Found',
-        content   : '<p>Not Found</p>',
-      },
-      wit: wit,
-    });
-  });
-
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
 });
 
 module.exports = app;
--- posts/hello-world.md
+++
@@ -1,9 +0,0 @@
-{{{
-"title"      : "Hello World!",
-"author"     : "John Doe",
-"categories" : [ "outro" ],
-"tags"       : [ ],
-"date"       : "2014-01-01"
-}}}
-
-You're now ready to begin. Happy blogging!
