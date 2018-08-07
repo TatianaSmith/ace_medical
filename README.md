329c64a575b759b1c9caa2f99073afff6d25c43d
Anton Mac <antony.mayer@gmail.com>
2018-08-06 22:07:28 -0500

    Update

--- README.md
+++ README.md
@@ -5,5 +5,6 @@ php -S localhost:8000 system/router.php
 ```
 
 ## Hints
+http://localhost:8000/admin
 tatiana.v.smith@gmail.com
 acemedical/K..110..id
---
+++ cache/compiled/files/1c1cb24e6deb08fcfdc75ce23508e3c4.yaml.php
@@ -0,0 +1,22 @@
+<?php
+return [
+    '@class' => 'Grav\\Common\\File\\CompiledYamlFile',
+    'filename' => '/Users/Tatiana/projects/ace_medical/user/accounts/acemedical.yaml',
+    'modified' => 1532993116,
+    'data' => [
+        'email' => 'tatiana.v.smith@gmail.com',
+        'fullname' => 'Tatsiana Kuzniatsova',
+        'title' => 'Admin',
+        'state' => 'enabled',
+        'access' => [
+            'admin' => [
+                'login' => true,
+                'super' => true
+            ],
+            'site' => [
+                'login' => true
+            ]
+        ],
+        'hashed_password' => '$2y$10$H2stCa/qwpzMLh0naqZYieDofx4MN/WLNyvfcEIbo0hdVcjDW25ka'
+    ]
+];
